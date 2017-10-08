4d5c3b19afb02a8de71e3872c50b927df2a1721b
Shuhaojie <2386677465@qq.com>
2017-10-08 17:00:50 +0800

    Update RSA.scala

--- RSA.scala
+++ RSA.scala
@@ -1,5 +1,3 @@
-package com.dt.encryption
-
 /**
   * Created by Jason Shu on 2017/8/5.
   */
@@ -10,10 +8,10 @@ import java.security.spec.X509EncodedKeySpec
 import scala.io.Source
 import org.apache.commons.codec.binary.Base64
 import javax.crypto._
-
 object RSA {
+ //将字节转化为十六进制
   def bytes2hex(bytes: Array[Byte]): String = {
-    val hex = new StringBuilder()
+    val hex = new StringBuilder()//StringBuilder将字符串连接在一起
     for (i <- 0 to bytes.length - 1) {
       val b = bytes(i)
       var negative = false
@@ -21,7 +19,7 @@ object RSA {
         negative = true
       }
       val inte = Math.abs(b)
-      val temp = Integer.toHexString(inte & 0xFF)
+      val temp = Integer.toHexString(inte & 0xFF)//显示一个byte型的单字节十六进制(两位十六进制表示)的编码
       if (temp.length() == 1) {
         hex.append("0")
       }
@@ -30,11 +28,13 @@ object RSA {
     }
     hex.toString
   }
+ //对字符串型公钥进行解密
   def decodePublicKey(encodedKey: String):Option[PublicKey] = {
     this.decodePublicKey(
       (new Base64()).decode(encodedKey)
     )
   }
+ //对字符数组型公钥进行解密
   def decodePublicKey(encodedKey: Array[Byte]): Option[PublicKey]= {
     scala.util.control.Exception.allCatch.opt {
       val spec = new X509EncodedKeySpec(encodedKey)
@@ -42,30 +42,19 @@ object RSA {
       factory.generatePublic(spec)
     }
   }
-
+ //定义加密方法
   def encrypt(file: String,key:PublicKey): Array[Byte] = {
-
     val listFile=Source.fromFile(file).toList
-
+   //本算法中采用的密钥长度为512bytes,rsa中能加密的最大字节数为密钥长度减11 
     val blocks=list.grouped(501).toArray
-
-    val cipher = Cipher.getInstance("RSA")
-
-    cipher.init(Cipher.ENCRYPT_MODE, key)
-
+    cipher.init(Cipher.getInstance("RSA").ENCRYPT_MODE, key)
     blocks.flatMap{block0 =>
       val stringBlock=block0.mkString
       cipher.doFinal(stringBlock.getBytes)}
-
   }
   def main(args:Array[String]):Unit={
-
-    val publicKey=decodePublicKey(publickey)
-
-    val cipher = encrypt(inputfile,publicKey.get)
-
-    val bytes2hexCipher=bytes2hex(cipher)
-    
-    println(bytes2hexCipher)
+    val publicKey=decodePublicKey(publickey)//public可以由密钥生成工具生成
+    val bytes2hexCipher=bytes2hex(encrypt(inputfile,publicKey.get))
+    new PrintWriter(new File(outputfile)).write(bytes2hexCipher)//由于RSA加密速度慢,所以最好采用PrintWriter的方式输出
   }
 }
